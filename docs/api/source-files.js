1196cb3b92078396cfe582a149017caf2ede6717
sagie gur ari <sagiegurari@gmail.com>
2019-05-09 10:47:25 +0300

    release

--- docs/api/source-files.js
+++ docs/api/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["ci_info"] = {"name":"","files":["ci.rs","config.rs","env.rs","lib.rs","types.rs"]};
+sourcesIndex["ci_info"] = {"name":"","files":["ci.rs","config.rs","lib.rs","types.rs"]};
 createSourceSidebar();
